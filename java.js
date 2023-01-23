48c0497afcd576526e2e072656fb05b7d94e102c
Dom <122333281+DomenicGTH@users.noreply.github.com>
2023-01-23 10:39:31 +0000

    Changed it to actually BE rock paper scissors

--- java.js
+++ java.js
@@ -1,10 +1,17 @@
 
+let games = 0
+let computer = 0
+let player = 0
+
 // function to choose between 3 numbers
+
+
 function computerchoice () {
     const randomNum = Math.floor(Math.random() * 3);
     
+
     if (randomNum == 0) {
-        return "Rock" 
+        return "Rock";
     }
     else if (randomNum == 1) {
         return "Paper";
@@ -13,20 +20,51 @@ function computerchoice () {
         return "Scissors";
     }
 }
-    function userInput () {
+    
+function userInput () {
        let input = prompt ("Rock, paper, scissors");
        let answer = input .charAt(0).toUpperCase() + input .slice(1).toLowerCase();
+       if (answer === "Rock") {
+        return "Rock";
+       }
+       else if (answer === "Paper") {
+        return "Paper";
+       }
+       else if (answer === "Scissors") {
+        return "Scissors";
+       }
+       else if (answer  )
        return answer;
     }
 
 
-    function comparison () {
-    
-     if (computerchoice() === userInput()) {
-         return ("YOU ARE THE WIN");
-    }
-        else return ("i win");
-    }
 
-    console.log(computerchoice());
-    console.log(comparison());
+    // Notes for my small brain 
+    // ? = ternary operator, compares the conditions
+    // : splits the conditions, example in line 50/51, will check if the user input is paper, and if not will say the other possible input
+    // flawed as a user can type anything and will win
+function comparison () {
+
+    if (computerchoice == userInput) {
+        return "draw"
+    }
+    else if (computerchoice() == "Rock") {
+        return (userInput() == "Paper") ? "I chose rock, Paper beats rock" : "I chose rock, Rock beats scissors"
+    }
+    
+    else if (computerchoice() == "Paper") {
+        return (userInput() == "Scissors") ? "I chose paper, Scissors beats paper" : "I chose paper, Paper beats rock"
+    }
+    else if (computerchoice() == "Scissors") {
+        return (userInput() == "Rock") ? "I chose Scissors, Rock beats Scissors" : "I chose scissors, Scissors beats paper"
+    }
+}
+
+console.log(comparison());
+
+
+        
+
+
+
+
