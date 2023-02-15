70eb1f9d7f67da05fffd242d84eeab6b4d9db869
Dom <122333281+DomenicGTH@users.noreply.github.com>
2023-02-15 04:38:29 +0000

    added basic ui funcionality

--- java-main.js
+++ java-main.js
@@ -1,19 +1,40 @@
 let computer = 0
 let player = 0
 let rounds = 0
-let draws = 0
+let draws
+let buttonanswer
+let computeranswer
+let drawsanswer = document.querySelector("#draws");
+let playerchoice = document.querySelector("#playerchoice");
+let computerscore = document.querySelector('#computerchoice')
+let results = document.querySelector('#results')
+let draw = document.querySelector('#draw')
+const buttonChoice = document.querySelectorAll('.choice');
+
+buttonChoice.forEach(button => button.addEventListener('click', () => {
+
+    buttonanswer = button.textContent;
+    computerchoice();
+    playerchoice.textContent = `Player chose ${buttonanswer} --- score: ${player}`;
+    computerscore.textContent = `Computer chose ${computeranswer} --- score: ${computer}`;
+    // draw.textContent = `Draws: ${draws}`;
+    results.textContent = comparison();
+}));
+    // btn.addEventListener('click', function(e){
+    //     buttonanswer = e.target.innerText;
+    //     console.log(buttonanswer);
 
 function comparison() {
 
-    let a = userInput()
-    let b = computerchoice()
+    let a = buttonanswer;
+    let b = computeranswer;
 
-    if (a === b) {
+    if (a == b) {
         draws++;
     } 
     
     if ((a === "Rock" && b === "Scissors") ||
-        (a === "Paper" && b === "Rock") ||
+        (a=== "Paper" && b === "Rock") ||
         (a === "Scissors" && b === "Paper")) {
         player++;
     
@@ -27,38 +48,41 @@ function comparison() {
          
 
 
-function scoretracker() {
-    for (rounds = 0; rounds < 10; rounds++) {
-        console.log('---')
-        console.log(computer, 'computer')
-        console.log(player, 'player')
-        console.log(rounds, 'rounds')
-        console.log(draws, 'draws')
-        comparison();
+// function scoretracker() {
+//     for (rounds = 0; rounds < 10; rounds++) {
+//         console.log('---')
+//         console.log(computer, 'computer')
+//         console.log(player, 'player')
+//         console.log(rounds, 'rounds')
+//         console.log(draws, 'draws')
+//         comparison();
 
-        if (rounds === 9) {
-            console.log("Game over");
-        }
-        if (computer === 5) {
-            rounds = 10
-            console.log("You lost! Total score = player -" + player + "- computer -" + computer + "- and draws -" + draws + "-");
-        }
-        if (player === 5) { 
-            rounds = 10
-            console.log("You won! Total score = player -" + player + "- computer -" + computer + "- and draws -" + draws + "-");
-        }
-    }
-}
+//         if (rounds === 9) {
+//             console.log("Game over");
+//         }
+//         if (computer === 5) {
+//             rounds = 10
+//             console.log("You lost! Total score = player -" + player + "- computer -" + computer + "- and draws -" + draws + "-");
+//         }
+//         if (player === 5) { 
+//             rounds = 10
+//             console.log("You won! Total score = player -" + player + "- computer -" + computer + "- and draws -" + draws + "-");
+//         }
+//     }
+// }
 
 function computerchoice () {
     const randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 0) {
+        computeranswer = "Rock"
         return "Rock";
     }
     else if (randomNum === 1) {
+        computeranswer = "Paper"
         return "Paper";
     }
     else if (randomNum === 2) {
+        computeranswer = "Scissors"
         return "Scissors";
     }
 }
@@ -77,4 +101,3 @@ function userInput () {
     return "Scissors";
    }
 }
-scoretracker()
